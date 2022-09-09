Fix sobel and unsharp masking, change images to morphologic operations

Questao4/letraA.py
@@ -1,9 +1,8 @@
 from PIL import Image
-import numpy as np 
 
 import sys        
 sys.path.append('D:\Programming\PDI\Trabalho2')
-from Questao3.letraA import union
+from Questao3.letraC import diff
 
 def pixelInsideImage(image, i, j):
     lines = image.size[0]
@@ -44,39 +43,18 @@ def dilation(image, kernel, maskCenter=None):
     return imageResult
 
 if __name__ == '__main__':
-    sizeImage = 10
-    image1 = Image.new("1", (sizeImage, sizeImage))
-    image1.putdata(np.ones(sizeImage*sizeImage))
-
-    for i in range(image1.size[0] // 2):
-        for j in range(image1.size[1] // 2):
-            image1.putpixel((i, j), 0)
-
-    image1.save("Trabalho2\Questao4\letraA\image1.png")
-
     mask = {}
     mask[0] = [0, 0, 0] 
     mask[1] = [0, 0, 0] 
     mask[2] = [0, 0, 0]  
 
-    sizeKernel = 3
+    image1 = Image.open("Trabalho2\Questao4\JImage.png")
+
     dilationImage = dilation(image1, mask)
 
     # dilationImage.show()
     dilationImage.save("Trabalho2\Questao4\letraA\imageResult.png")
 
-    diffImage = Image.new("1", (sizeImage, sizeImage))
-    for i in range(image1.size[0]):
-        for j in range(image1.size[1]):
-            diff = image1.getpixel((i, j)) - dilationImage.getpixel((i, j))
-            diffImage.putpixel((i, j), diff)
+    diffImage = diff(image1, dilationImage)
     
     diffImage.save("Trabalho2\Questao4\letraA\diffImage.png")
-
-    proveDiff = Image.new("1", (sizeImage, sizeImage))
-    for i in range(image1.size[0]):
-        for j in range(image1.size[1]):
-            diff = diffImage.getpixel((i, j)) + dilationImage.getpixel((i, j))
-            proveDiff.putpixel((i, j), diff)
-    
-    proveDiff.save("Trabalho2\Questao4\letraA\proveDiff.png")

Questao4/letraB.py
@@ -1,6 +1,9 @@
 from PIL import Image
-import numpy as np 
 
+import sys        
+sys.path.append('D:\Programming\PDI\Trabalho2')
+
+from Questao3.letraC import diff
 from Questao4.letraA import pixelInsideImage
 
 def erosion(image, kernel):
@@ -32,15 +35,7 @@ def erosion(image, kernel):
     return imageResult
 
 if __name__ == '__main__':
-    sizeImage = 10
-    image1 = Image.new("1", (sizeImage, sizeImage))
-    image1.putdata(np.ones(sizeImage*sizeImage))
-
-    for i in range(image1.size[0] // 2):
-        for j in range(image1.size[1] // 2):
-            image1.putpixel((i, j), 0)
-
-    image1.save("Trabalho2\Questao4\letraB\image1.png")
+    image1 = Image.open("Trabalho2\Questao4\JImage.png")
 
     mask = {}
     mask[0] = [0, 0, 0] 
@@ -53,18 +48,6 @@ if __name__ == '__main__':
     # erosionImage.show()
     erosionImage.save("Trabalho2\Questao4\letraB\imageResult.png")
 
-    diffImage = Image.new("1", (sizeImage, sizeImage))
-    for i in range(image1.size[0]):
-        for j in range(image1.size[1]):
-            diff = image1.getpixel((i, j)) - erosionImage.getpixel((i, j))
-            diffImage.putpixel((i, j), diff)
+    diffImage = diff(image1, erosionImage)
     
     diffImage.save("Trabalho2\Questao4\letraB\diffImage.png")
-
-    proveDiff = Image.new("1", (sizeImage, sizeImage))
-    for i in range(image1.size[0]):
-        for j in range(image1.size[1]):
-            diff = diffImage.getpixel((i, j)) + erosionImage.getpixel((i, j))
-            proveDiff.putpixel((i, j), diff)
-    
-    proveDiff.save("Trabalho2\Questao4\letraB\proveDiff.png")

Questao4/letraC.py
@@ -4,6 +4,10 @@ import numpy as np
 from letraA import dilation
 from letraB import erosion
 
+import sys        
+sys.path.append('D:\Programming\PDI\Trabalho2')
+from Questao3.letraC import diff
+
 def opening(image, kernel):
     erosionImage = erosion(image, kernel)
     erosionImage.save("Trabalho2\Questao4\letraC\erosionImage.png")
@@ -14,39 +18,15 @@ def opening(image, kernel):
     return openingImage
 
 if __name__ == '__main__':
-    sizeImage = 10
-    image1 = Image.new("1", (sizeImage, sizeImage))
-    image1.putdata(np.ones(sizeImage*sizeImage))
-
-    for i in range(image1.size[0] // 2):
-        for j in range(image1.size[1] // 2):
-            image1.putpixel((i, j), 0)
-
-    image1.save("Trabalho2\Questao4\letraC\image1.png")
+    image1 = Image.open("Trabalho2\Questao4\imageToOpening.png")
 
     mask = {}
     mask[0] = [0, 0, 0] 
     mask[1] = [0, 0, 0] 
     mask[2] = [0, 0, 0]  
 
-    sizeKernel = 3
     openingImage = opening(image1, mask)
-
-    # openingImage.show()
     openingImage.save("Trabalho2\Questao4\letraC\imageResult.png")
 
-    diffImage = Image.new("1", (sizeImage, sizeImage))
-    for i in range(image1.size[0]):
-        for j in range(image1.size[1]):
-            diff = image1.getpixel((i, j)) - openingImage.getpixel((i, j))
-            diffImage.putpixel((i, j), diff)
-    
+    diffImage = diff(image1, openingImage)
     diffImage.save("Trabalho2\Questao4\letraC\diffImage.png")
-
-    proveDiff = Image.new("1", (sizeImage, sizeImage))
-    for i in range(image1.size[0]):
-        for j in range(image1.size[1]):
-            diff = diffImage.getpixel((i, j)) + openingImage.getpixel((i, j))
-            proveDiff.putpixel((i, j), diff)
-    
-    proveDiff.save("Trabalho2\Questao4\letraC\proveDiff.png")

Questao4/letraD.py
@@ -1,52 +1,42 @@
 from PIL import Image
-import numpy as np 
+
+import sys        
+sys.path.append('D:\Programming\PDI\Trabalho2')
 
 from Questao4.letraA import dilation
 from Questao4.letraB import erosion
+from Questao3.letraC import diff
 
 def closing(image, kernel):
     dilatadedImage = dilation(image, kernel)
-    dilatadedImage.save("Trabalho2\Questao4\letraD\dilatadedImage.png")
 
     closingImage = erosion(dilatadedImage, kernel)
-    closingImage.save("Trabalho2\Questao4\letraD\closingImage.png")
     
     return closingImage
 
 if __name__ == '__main__':
-    sizeImage = 10
-    image1 = Image.new("1", (sizeImage, sizeImage))
-    image1.putdata(np.ones(sizeImage*sizeImage))
+    image1 = Image.open("Trabalho2\Questao4\imageToClosing.png")
 
-    for i in range(image1.size[0] // 2):
-        for j in range(image1.size[1] // 2):
-            image1.putpixel((i, j), 0)
+    mask3x3 = {}
+    mask3x3[0] = [0, 0, 0] 
+    mask3x3[1] = [0, 0, 0] 
+    mask3x3[2] = [0, 0, 0]  
 
-    image1.save("Trabalho2\Questao4\letraD\image1.png")
+    mask5x5 = {}
+    mask5x5[0] = [0, 0, 0, 0, 0]
+    mask5x5[1] = [0, 0, 0, 0, 0]
+    mask5x5[2] = [0, 0, 0, 0, 0]
+    mask5x5[3] = [0, 0, 0, 0, 0]
+    mask5x5[4] = [0, 0, 0, 0, 0]
 
-    mask = {}
-    mask[0] = [0, 0, 0] 
-    mask[1] = [0, 0, 0] 
-    mask[2] = [0, 0, 0]  
+    closingImage = closing(image1, mask3x3)
+    closingImage.save("Trabalho2\Questao4\letraD\imageResult3x3.png")
 
-    sizeKernel = 3
-    closingImage = closing(image1, mask)
+    diffImage = diff(image1, closingImage)
+    diffImage.save("Trabalho2\Questao4\letraD\diffImage3x3.png")
 
-    # closingImage.show()
-    closingImage.save("Trabalho2\Questao4\letraD\imageResult.png")
+    closingImage = closing(image1, mask5x5)
+    closingImage.save("Trabalho2\Questao4\letraD\imageResult5x5.png")
 
-    diffImage = Image.new("1", (sizeImage, sizeImage))
-    for i in range(image1.size[0]):
-        for j in range(image1.size[1]):
-            diff = image1.getpixel((i, j)) - closingImage.getpixel((i, j))
-            diffImage.putpixel((i, j), diff)
-    
-    diffImage.save("Trabalho2\Questao4\letraD\diffImage.png")
-
-    proveDiff = Image.new("1", (sizeImage, sizeImage))
-    for i in range(image1.size[0]):
-        for j in range(image1.size[1]):
-            diff = diffImage.getpixel((i, j)) + closingImage.getpixel((i, j))
-            proveDiff.putpixel((i, j), diff)
-    
-    proveDiff.save("Trabalho2\Questao4\letraC\proveDiff.png")
+    diffImage = diff(image1, closingImage)
+    diffImage.save("Trabalho2\Questao4\letraD\diffImage5x5.png")